53497bf2c0b042cf8c3066182dc0e45343ce8191
AlexAntoine <antoinealex1234@gmial.com>
2021-10-07 02:14:57 -0400

    added prints statements for db connect functions

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,12 +5,14 @@ const prodDb = () =>{
     
     mongoose.set('useCreateIndex', true);
     mongoose.connect(process.env.PROD_DB, {useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:true });
+    console.log('prod db is connected')
 }
 
 const devDb = () =>{
 
     mongoose.set('useCreateIndex', true);
     mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:true });
+    console.log('dev db is connected');
 }
 
 module.exports = {
